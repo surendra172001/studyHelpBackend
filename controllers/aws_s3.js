ff1e48006001ca4899190caae3e997bedc1c8495
Surendra Pandey <sap172001@gmail.com>
2022-02-13 03:23:38 +0530

    Added public access

--- controllers/aws_s3.js
+++ controllers/aws_s3.js
@@ -15,6 +15,7 @@ const uploadS3 = multer({
   storage: multerS3({
     s3: s3,
     bucket: process.env.AWS_BUCKET_NAME,
+    acl: "public-read",
     metadata: function (req, file, cb) {
       cb(null, { fieldName: file.fieldname });
     },
